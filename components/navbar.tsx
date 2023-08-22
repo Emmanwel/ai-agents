acb2230bc34ad9de0411a66911be335c4bc684b2
Emmanwel <mukhebiemmanuel5@gmail.com>
2023-08-22 13:56:12 +0300

    fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 import { Poppins } from "next/font/google";
 import { Sparkles } from "lucide-react";
-import Link from 'next/link';
 
 import { cn } from "@/lib/utils";
 import { MobileSidebar } from "@/components/mobile-sidebar";
@@ -24,21 +24,21 @@ export const Navbar = ({
   return ( 
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
       <div className="flex items-center">
-        <MobileSidebar />
+        <MobileSidebar isPro={isPro} />
         <Link href="/">
           <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
-            Omic Companion Model
+            AI Agents
           </h1>
         </Link>
       </div>
       <div className="flex items-center gap-x-3">
-        {!isPro && ( 
+        {!isPro && (
           <Button onClick={proModal.onOpen} size="sm" variant="premium">
-             Upgrade
-             <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
-           </Button>
-         )}         
-              <ModeToggle />
+            Upgrade
+            <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+          </Button>
+        )}
+        <ModeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
     </div>
