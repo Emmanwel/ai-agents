390f48e8e250ef9eeed7d71cdc7defd0a80868f6
Emmanwel <mukhebiemmanuel5@gmail.com>
2023-08-22 13:50:24 +0300

    mobile-sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,9 +4,9 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "@/components/sidebar";
 
 export const MobileSidebar = ({
-//   isPro
+  isPro
 }: {
-//   isPro: boolean;
+  isPro: boolean;
 }) => {
   return (
     <Sheet>
@@ -14,8 +14,7 @@ export const MobileSidebar = ({
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        {/* <Sidebar isPro={isPro} /> */}
-          <Sidebar  />
+        <Sidebar isPro={isPro} />      
       </SheetContent>
     </Sheet>
   );
